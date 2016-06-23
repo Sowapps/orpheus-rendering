d939af442ae8568a3f07e659d291d288e255cc2f
Loenix <florent@sowapps.com>
2016-06-23 15:31:22 +0200

    First commit, imported from Oprheus project with PSR4 fix
    Use official route resolver

---
+++ src/Rendering/RawRendering.php
@@ -0,0 +1,23 @@
+<?php
+namespace Orpheus\Rendering;
+
+/**
+ * The raw rendering class
+ * 
+ * A class to render module display without any treatment.
+ */
+class RawRendering extends Rendering {
+	
+	/**
+	 * Render the model.
+	 * 
+	 * @see Rendering::render()
+	 */
+	public function render($model=null, $env=array()) {
+		extract($env);
+		if( !isset($Page) ) {
+			$Page = '';
+		}
+		return $Page;
+	}
+}
